3715b31eb51ee884981c336440d1703709c812d2
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2023-09-10 16:13:50 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,140 @@
+# TypeShields
+
+Add runtime type checking to your Javascript code without writing a bunch of boilerplate.
+
+### Shields
+
+- `any`
+    - Does not do any checks.
+- `unit`
+    - Requires the checked value to be `undefined`.
+- `number`
+    - Requires the checked value to be a number.
+- `string`
+    - Requires the checked value to be a string.
+- `boolean`
+    - Requires the checked value to be a boolean.
+- `symbol`
+    - Requires the value to be a symbol.
+- `instance(c)`
+    - Requires the value to be an instance of the provided class `c`.
+- `array(e)`
+    - Requires the value to be an array.
+    - `e` is expected to be a type shield where every element in the checked value is checked against `e`.
+- `object(m)`
+    - Requires the value to be an object.
+    - `m` is expected to be an object where every member has to be a type shield, and all members of the checked value are then checked against the type shields stored in the member of `m` with the same name.
+- `func(p, r)`
+    - Requires the value to be a function.
+    - `p` is expected to be an array where every element has to be a type shield, and (when the function is called) every parameter value will be checked against the type shield stored in `p` at the same index.
+    - `r` is expected to a type shield where (when the function is called) the return type will be checked against `r`.
+
+### How to use
+
+To check a value against a type shield, call the type shield and pass the value as the only parameter. The value will be returned.
+```js
+let pi = number(3.14);
+let tau = (number) (6.28); // if you want it to look like a C type cast
+```
+<br>
+
+If the type is invalid, an error will be thrown at runtime:
+```js
+let g = (string) (9.81);
+```
+```
+type mismatch: expected type 'string', got 'number' instead
+```
+<br>
+
+The`instance`-shield accepts the class that the value must be an instance of:
+```js
+let a = (instance(Map)) (new Map());
+let b = (instance(Map)) (25); 
+```
+```
+Uncaught type mismatch: expected type 'instance(Map)', got 'object' instead
+```
+<br>
+
+The `array`-shield accepts the type that is required for each element:
+```js
+let abc = (array(string)) (["A", "B", "C", "D", "E"]);
+let primes = (array(number)) ([1, 2, 3, 5, 7, "11", 13]);
+```
+```
+Uncaught type mismatch (6th element of 'array(number)'): expected type 'number', got 'string' instead
+```
+<br>
+
+The `object`-shield accepts the types that each member is required to have:
+```js
+let book = (object({ name: string, pages: number })) ({
+    name: "The Art of War",
+    pages: 260
+});
+
+const Cat = object({
+    name: string,
+    age: number
+});
+
+let cookie = (Cat) ({
+    name: "Cookie",
+    hunger: 0.5
+});
+```
+```
+type mismatch (member 'age' of 'object {name: string, age: number}'): expected type 'number', got 'undefined' instead
+```
+<br>
+
+The `func`-shield accepts a list of parameter types and a return type. When passed a function, the function it returns isn't the same - it has type checking built into it!
+```js
+const add = (func([number, number], number)) ((a, b) => a + b);
+add("Hello, ", 10);
+```
+```
+Uncaught type mismatch (1st parameter of 'func([number, number], number)'): expected type 'number', got 'string' instead
+```
+If you want to check a function to have no parameters, then make the parameter type list an empty array.
+To check a function to return nothing, make the return type `unit`.
+<br>
+
+The `any`-shield can come in handy when you need to provide a shield, but don't want to do any checks. For example, it can be used to check for an array that contains values of any type:
+```js
+let stuff = (array(any)) ([3, 8, "Hi", null, {}, 42]);
+```
+Or maybe you want an object that has to have a property, but the type of that property does not matter:
+```js
+const Container = object({
+    value: any
+});
+
+let c = (Container) ({
+    value: "Hello!"
+});
+```
+
+### Generics
+
+If you want to model a generic object type, use this pattern, where you define a function that takes one or more shields as parameters, and then return the resulting shield:
+```js
+const Box = (T) => object({
+    value: T
+});
+
+let a = (Box(number)) ({ value: 65536 });
+let b = (Box(string)) ({ value: "Hi!" });
+```
+<br>
+
+And for generic functions, use the same pattern, but instead return the function itself:
+```js
+const push_onto_array = (T) => (func([array(T), T], unit)) ((arr, v) => {
+    arr.push(v);
+});
+
+let a = [0, 1, 2, 3];
+push_onto_array(number)(a, 4);
+```
---
+++ typeshields.js
@@ -0,0 +1,113 @@
+
+const _TYPE_SHIELD_DEFINE = (typeof_name, name) => {
+    const checker = (value) => {
+        if(typeof_name !== undefined && typeof value !== typeof_name) {
+            throw `type mismatch: expected type '${name}', got '${typeof value}' instead`;
+        }
+        return value;
+    };
+    checker._TYPE_SHIELD_AS_STRING = name;
+    return checker;
+};
+
+const any = _TYPE_SHIELD_DEFINE(undefined, "any");
+
+const unit = _TYPE_SHIELD_DEFINE("undefined", "unit");
+
+const number = _TYPE_SHIELD_DEFINE("number", "number");
+
+const string = _TYPE_SHIELD_DEFINE("string", "string");
+
+const boolean = _TYPE_SHIELD_DEFINE("boolean", "boolean");
+
+const symbol = _TYPE_SHIELD_DEFINE("symbol", "symbol");
+
+const object = (member_types) => {
+    if(typeof member_types !== "object") { throw "The provided member types are invalid!"; }
+    const checker = (value) => {
+        if(typeof value !== "object") { throw "The value is not an object!"; }
+        for(const member_name of Object.keys(member_types)) {
+            try {
+                if(!value.hasOwnProperty(member_name)) { throw null; }
+                (member_types[member_name]) (value[member_name]);
+            } catch(e) {
+                throw `type mismatch (member '${member_name}' of '${checker._TYPE_SHIELD_AS_STRING}'): expected type '${member_types[member_name]._TYPE_SHIELD_AS_STRING}', got '${typeof value[member_name]}' instead`;
+            }
+        }
+        return value;
+    };
+    checker._TYPE_SHIELD_AS_STRING = `object {${Object.keys(member_types).map((member_name) => `${member_name}: ${member_types[member_name]._TYPE_SHIELD_AS_STRING}`).join(", ")}}`;
+    return checker;
+};
+
+const func = (param_types, return_type) => {
+    if(!(param_types instanceof Array)) { throw "The provided parameter types are invalid!"; }
+    if(typeof return_type !== "function") { throw "The provided return type is invalid!"; }
+    const checker = (value) => {
+        if(typeof value !== "function") { throw "The value is not a function!"; }
+        if(value._TYPE_SHIELD_IS_CHECKER_WRAPPER === true) { return value; }
+        const wrapper = (...arguments) => {
+            if(arguments.length !== param_types.length) { throw `type mismatch: function '${checker._TYPE_SHIELD_AS_STRING}' expected ${param_types.length} argument${param_types.length === 1? "" : "s"}, got ${arguments.length} instead`; }
+            for(let p = 0; p < param_types.length; p += 1) {
+                try {
+                    (param_types[p]) (arguments[p]);
+                } catch(e) {
+                    const dp = p + 1;
+                    const pf = dp % 10 === 1? "st" : dp % 10 === 2? "nd" : dp % 10 === 3? "rd" : "th";
+                    throw `type mismatch (${dp}${pf} parameter of '${checker._TYPE_SHIELD_AS_STRING}'): expected type '${param_types[p]._TYPE_SHIELD_AS_STRING}', got '${typeof arguments[p]}' instead`;
+                }
+            }
+            const returned = value(...arguments);
+            try {
+                (return_type) (returned);
+            } catch(e) {
+                throw `type mismatch (return value of '${checker._TYPE_SHIELD_AS_STRING}'): expected type '${return_type._TYPE_SHIELD_AS_STRING}', got '${typeof returned}' instead`;
+            }
+            return returned;
+        };
+        wrapper._TYPE_SHIELD_IS_CHECKER_WRAPPER = true;
+        return wrapper;
+    };
+    checker._TYPE_SHIELD_AS_STRING = `func([${param_types.map((t) => t._TYPE_SHIELD_AS_STRING).join(", ")}], ${return_type._TYPE_SHIELD_AS_STRING})`
+    return checker;
+};
+
+const array = (content_type) => {
+    if(typeof content_type !== "function") { throw "The provided content type is invalid!"; }
+    const checker = (value) => {
+        if(value.constructor !== Array) { throw `type mismatch: expected type '${checker._TYPE_SHIELD_AS_STRING}', got '${typeof value}' instead`; }
+        for(let i = 0; i < value.length; i += 1) {
+            try {
+                (content_type) (value[i]);
+            } catch(e) {
+                const di = i + 1;
+                const pf = di % 10 === 1? "st" : di % 10 === 2? "nd" : di % 10 === 3? "rd" : "th";
+                throw `type mismatch (${di}${pf} element (index ${i}) of '${checker._TYPE_SHIELD_AS_STRING}'): expected type '${content_type._TYPE_SHIELD_AS_STRING}', got '${typeof value[i]}' instead`;
+            }
+        }
+        return value;
+    };
+    checker._TYPE_SHIELD_AS_STRING =`array(${content_type._TYPE_SHIELD_AS_STRING})`;
+    return checker;
+}
+
+const instance = (class_constructor) => {
+    if(typeof content_type !== "function") { throw "The provided class is invalid!"; }
+    const checker = (value) => {
+        if(value instanceof class_constructor) { throw `type mismatch: expected type '${checker._TYPE_SHIELD_AS_STRING}', got '${typeof value}' instead`; }
+        return value;
+    };
+    checker._TYPE_SHIELD_AS_STRING =`instance(${class_constructor.name})`;
+    return checker;
+};
+
+
+if(typeof module !== "undefined") { module.exports = { any, unit, number, string, object, func, array }; }
+
+
+const push_onto_array = (T) => (func([array(T), T], unit)) ((arr, v) => {
+    arr.push(v);
+});
+
+let a = [0, 1, 2, 3];
+push_onto_array(number)(a, 4);
